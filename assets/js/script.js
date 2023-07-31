58134072af185a438c644972147002b38e001089
Artem Usatyi <miezaruexpr@gmail.com>
2023-07-31 23:33:04 +0300

    Website | First responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,30 @@
-// Sticky navigation
-const navMakesSticky = document.querySelector('.nav-makes-sticky')
-const navEl = document.querySelector('.page-nav')
-
-const obs = new IntersectionObserver(
-    function (entries) {
-        const ent = entries[0]
-        if (!ent.isIntersecting) {
-            navEl.classList.add('sticky-nav')
-        }
-        if (ent.isIntersecting) {
-            navEl.classList.remove('sticky-nav')
-        }
-    },
-    {
-        root: null, // observe inside of viewport
-        threshold: 0, // as soon as 0% section on a viewport
-        rootMargin: '-90px',
+// page navigation start
+function navigation() {
+    const navMakesSticky = document.querySelector('.nav-makes-sticky')
+    const navEl = document.querySelector('.page-nav')
+    if (window.innerWidth > 1100) {
+        // Sticky navigation
+        const obs = new IntersectionObserver(
+            function (entries) {
+                const ent = entries[0]
+                if (!ent.isIntersecting) {
+                    navEl.classList.add('sticky-nav')
+                }
+                if (ent.isIntersecting) {
+                    navEl.classList.remove('sticky-nav')
+                }
+            },
+            {
+                root: null, // observe inside of viewport
+                threshold: 0, // as soon as 0% section on a viewport
+                rootMargin: '-90px',
+            }
+        )
+        obs.observe(navMakesSticky)
+    } else if (window.innerWidth <= 1100) {
+        navEl.classList.remove('sticky-nav')
     }
-)
-obs.observe(navMakesSticky)
-
+}
 // details
 const details = document.querySelectorAll('.details')
 const detailsLinks = document.querySelectorAll('.details-link')
@@ -35,3 +40,7 @@ for (let i = 0; i < detailsLinks.length; i++) {
         }
     })
 }
+
+window.addEventListener('load', navigation, false)
+window.addEventListener('resize', navigation, false)
+// page navigation end
